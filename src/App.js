d67ff784d426527c806c4ece0afd674f09823317
Juan-Pablo-EAN <jballes89830@universidadean.edu.co>
2021-11-17 12:55:34 -0500

    creador de memes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,72 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import html2canvas from 'html2canvas';
 
 function App() {
 
-  var saludo = "Hola, soy Juan Pablo";
-  var texto = "Esta es mi primera app en React";
+  const [linea1, setLinea1] = useState(); //esto lo que hace es crear una variable (linea1) con un valor por defecto en los () de useState
+  //setLinea1 es como una funcion que ayuda a cambiar el valor de linea1
+  const [linea2, setLinea2] = useState();
+  const [imagen, setImagen] = useState();
+
+  const onChangeLinea1 = evento1 => { //el parametro se recibe automaticamente sin enviarlo al llamar la funcion. El parametro es el evento
+    setLinea1(evento1.target.value); //para obtener el valor del input con el evento se usa target.value
+    //setLinea1 es la funcion que creamos para la variable linea1
+  }
+
+  const onChangeLinea2 = evento2 => {
+    setLinea2(evento2.target.value);
+  }
+
+  const onChangeSelect = eventoS => {
+    setImagen(eventoS.target.value);
+  }
+
+  const exportarImagen = () => {
+    html2canvas(document.querySelector("#meme")).then(canvas => {
+      let img = canvas.toDataURL("image/png");
+      let link = document.createElement("a");
+      link.download = 'meme.png';
+      link.href = img;
+      link.click();
+    });
+    //parra esto se uso html2canvas que convierte el html en imagen
+    //el html2canvas se descarga dentro del proyecto con npm install --save html2canvas
+  }
+
+  let retorno =
+    <div className="App">
+      <select onChange={onChangeSelect}>
+        <option>Elige un meme</option>
+        <option value="fraid">Fraid</option>
+        <option value="gato">Gato conduciendo</option>
+        <option value="gato-malo">Gato malvado</option>
+        <option value="smigol">Esmigol</option>
+        <option value="inteligente">Inteligente ¿no?</option>
+      </select>
+      <br />
+      <input onChange={onChangeLinea1} type="text" placeholder="Linea superior" />
+      <br />
+      <input onChange={onChangeLinea2} type="text" placeholder="Linea inferior" />
+      <br />
+
+
+      <div className="contenedorMeme">
+        <div id="meme">
+          <span className="lineaS">{linea1}</span>
+          <br />
+          <span className="lineaI">{linea2}</span>
+          <br />
+          <img className="meme" src={"/img/" + imagen + ".jpg"} alt={"Meme de " + imagen} />
+        </div>
+      </div>
+      <br />
+      <button onClick={exportarImagen}>Exportar</button>
+    </div>;
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>{saludo}</h1>
-        <p>f
-          {texto}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    retorno
   );
 }
 
